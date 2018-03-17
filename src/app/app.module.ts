c279b548554fda3785ab5f51ac0ca6f434c5e47f
unknown <vaibhavpadalia07@gmail.com>
2018-03-18 02:53:27 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { MessagingService } from './messaging.service';
+import { AngularFireModule } from 'angularfire2';
+// for AngularFireDatabase
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
+// for AngularFireAuth
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { routingComponents, AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, routingComponents],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyBfgMuhIpfMvcE04lyHvHhSnAiGFa8IXKM',
+      authDomain: 'push-notifications-9cf18.firebaseapp.com',
+      databaseURL: 'https://push-notifications-9cf18.firebaseio.com',
+      projectId: 'push-notifications-9cf18',
+      storageBucket: 'push-notifications-9cf18.appspot.com',
+      messagingSenderId: '233910781322'
+    }),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [MessagingService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,8 @@
+importScripts("https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js");
+
+firebase.initializeApp({
+  messagingSenderId: '233910781322'
+});
+
+const messaging = firebase.messaging();
