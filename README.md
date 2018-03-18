22acc8f689cc1893e44abdef98236229eb7775e8
vaibhavpadalia <vaibhavpadalia07@gmail.com>
2018-03-18 19:07:03 +0530

    REAMDME.md added

---
+++ README.md
@@ -0,0 +1,12 @@
+# 📌 Push Notification
+
+This is an example of implementing Push Notifications of FCM in Angular.
+
+<img src = "https://cdn-images-1.medium.com/max/840/1*wiNrwnAnwjBzbjlXUgrj6A.png">
+
+At first install two packages:
+* firebase
+* angularfire2
+
+`npm install firebase angularfire2 --save`
+
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js");
 
 firebase.initializeApp({
-  messagingSenderId: '233910781322'
+  messagingSenderId: "233910781322"
 });
 
 const messaging = firebase.messaging();
